9da6c41b76febf0f0d68b62a12059fbbae991cb2
mysoyul <vega2k_@naver.com>
2021-11-23 11:26:32 +0900

    post edit

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,13 @@ from .models import Post
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        pass
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostModelForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form':form})

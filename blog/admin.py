e676515558ea5c111e78cbbaa516794985168ad7
mysoyul <vega2k_@naver.com>
2021-11-23 09:13:41 +0900

    admin page customize

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'count_text']
+    list_display_links = ['title']
+
+    def count_text(self, post):
+        return f'{len(post.text)}글자'
+
+    count_text.short_description = 'text글자수'
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
